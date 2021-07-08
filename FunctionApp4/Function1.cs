b50f806008ea17e64a3aea5dd32e9d8beea55612
kalluribhavani <81612597+kalluribhavani@users.noreply.github.com>
2021-07-08 12:23:40 +0530

    Update Function1.cs
    updated log information

--- FunctionApp4/Function1.cs
+++ FunctionApp4/Function1.cs
@@ -18,7 +18,7 @@ namespace FunctionApp4
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
-
+            llog.LogInformation("C#");
             string name = req.Query["name"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
